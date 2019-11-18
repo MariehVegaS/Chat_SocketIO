50a950a4105d0a1821ee8c57a0203cab58e34190
MariehVegaS <marievalentinelove@gmail.com>
2019-11-18 13:55:53 -0500

    Interfaz de registro, cambio de rutas con algunos botones

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const router = express.Router();
 const port = process.env.PORT || 3000;
-const http = require('http').createServer(app);
+
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
   res.sendFile(`${__dirname}/public/views/Inicio.html`,(e)=>{
-      //res.send('Error');
   });
+});
+
+app.get('/registro', function(req, res){
+    res.sendFile(`${__dirname}/public/views/Registro.html`,(e)=>{
+    });
+});
+
+app.get('/chatroom/username', function(req, res){
+    res.sendFile(`${__dirname}/public/views/Chatroom.html`,(e)=>{
+    });
 });
 
 http.listen(port, function(){

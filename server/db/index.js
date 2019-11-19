78650a89a6fffa4025d31a1d71322f560d244c54
MariehVegaS <marievalentinelove@gmail.com>
2019-11-18 22:03:51 -0500

    Creación de base de datos, conexión de la BD con el proyecto, validación de datos para usuario (básico)

---
+++ server/db/index.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    user: 'root',
+    database: 'chat',
+    host: 'localhost',
+    port: '3306'
+});
+
+let chatdb = [];
+
+chatdb.createDB = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`CREATE SCHEMA chat;
+            CREATE TABLE chat.usuarios (
+            id INT NOT NULL,
+            user VARCHAR(45) NOT NULL,
+            password VARCHAR(45) NOT NULL,
+            PRIMARY KEY (id));`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+}
+
+chatdb.all = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM usuarios`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+chatdb.one = (user) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT password FROM usuarios WHERE user = ?`,[user], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+module.exports = chatdb;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const db = require('../db');
 const router = express.Router();
+let password;
 
 router.get('/', (req, res, next) => {
     res.sendFile(`${process.cwd()}/public/views/Inicio.html`, (e) => {
@@ -11,9 +13,39 @@ router.get('/registro', (req, res, next) => {
     });
 });
 
-router.get('/chatroom', (req, res, next) => {
+router.get('/chatroom/:username', (req, res, next) => {
     res.sendFile(`${process.cwd()}/public/views/Chatroom.html`, (e) => {
     });
+});
+
+router.get('/validation/:username/:password', async (req, res, next) => {
+    try {
+        let all = await db.all();
+        let username = req.params.username;
+        let exist = false;
+        all.forEach(element => {
+            if (element.user == username) {
+                exist = true;
+            }
+        });
+        if (exist == true) {
+            let results = await db.one(username);
+            let password = req.params.password;
+            let passwordDB = results[0].password;
+            if (password == passwordDB) {
+                res.redirect(`http://localhost:3000/chatroom/${req.params.username}`);
+            } else {
+                console.log('Contraseña incorrecta');
+                res.redirect(`http://localhost:3000/`);
+            }
+        }else{
+            console.log('El usuario no existe');
+            res.redirect(`http://localhost:3000/`);
+        }
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
